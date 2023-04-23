8032f7b2e59ef6ea6c821c56744cceb9ec3271cc
SkidRow <51795604+RolandoHidalgo@users.noreply.github.com>
2023-04-22 23:48:25 -0400

    init

---
+++ src/main/java/com/example/oauthpro/OauthProApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauthpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oauthpro/OauthProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauthpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
